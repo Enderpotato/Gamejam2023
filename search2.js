023d4d2fba5548bc2f595ee6cb4001d9399dfdcb
guanyuehan <130543616+guanyuehan@users.noreply.github.com>
2023-12-12 17:28:29 +0800

    added hostile and passive
    how did this take 30 minutes

--- search2.js
+++ search2.js
@@ -12,30 +12,54 @@ import { ghost } from "./sceneSetup.js";
 import { castRay } from "./helperFuncs/raycast.js";
 
 export default class Direction{
-    constructor(){
+    constructor(n){
+        this.isHostile = true;
         this.velocity = new Vector3(0, 0, 0)
-        this.vectors = new Array(8);
-        this.directions = [new Vector3(0, 0, 1) ,
-                        new Vector3(1, 0, 1) ,
-                        new Vector3(0, 0, 1) ,
-                        new Vector3(1, 0, -1) ,
-                        new Vector3(0, 0, -1) ,
-                        new Vector3(-1, 0, -1),
-                        new Vector3(-1, 0, 0) ,
-                        new Vector3(-1, 0, 1)]
+        this.vectors = new Array(n);
+        this.directions = [];
+        let deltaDirVector = 2* Math.PI / n;
+        for (let i = 0; i < n; i++){
+          this.directions.push(new Vector3(Math.cos(i * deltaDirVector), 0, Math.sin(i * deltaDirVector)));
+        }
     }
 }
 
-Direction.prototype.getDirection = function(ghostPosition, playerPostion){ 
+Direction.prototype.getDirection = function(ghostPosition, playerPostion, ghostFacing){ 
   let avg_vector = new Vector3(0, 0, 0);
+  let relativePosition  = ghostPosition.subtract(playerPostion);
+  let relativeVectorOfDirectionsToPlayer = new Array(this.vectors.length);
+  let minLength;
+  let minLengthIndex;
   for (let i = 0; i < this.vectors.length; i++){
     this.vectors[i] = get_weighted_vector(castRay(this.directions[i], ghostPosition).rayLength, this.directions[i]);
     avg_vector.add_(this.vectors[i]);
+    //calculate which vector the ghost should evaluate to check if player is hidden
+    //we have to get the vector closest to where the ghost is facing
+    //to do that, we will find the direction vector in which is has a minimum length to the player.
+    //We must note that the directions vector is relative, so we use relativePosition
+    relativeVectorOfDirectionsToPlayer[i] = relativePosition.subtract(this.directions[i]).mag();
   }
-  let relativePosition = ghostPosition.subtract(playerPostion);
+
+  // console.log(relativeVectorOfDirectionsToPlayer)
+
+  minLength = Math.min(...relativeVectorOfDirectionsToPlayer);
+  minLengthIndex = 15 - int(relativeVectorOfDirectionsToPlayer.indexOf(minLength));
+
+  if (castRay(this.directions[minLengthIndex], ghostPosition).rayLength < relativePosition.mag()){
+    this.isHostile = false;
+  } else {
+    this.isHostile = true;
+  }
+  // console.log(this.isHostile)
+
+  if (this.isHostile){
+    relativePosition = ghostPosition.subtract(playerPostion);
+  } else {
+    relativePosition = new Vector3(0, 0, 0);
+  }
   avg_vector.y = 0;
   relativePosition.y = 0;
-  avg_vector.subtract_(relativePosition.elementMult(10));
+  avg_vector.subtract_(relativePosition.elementMult(5));
 
   return avg_vector;
 }
